98ece014bb0b7bbf664e5f90ea3d4849205c860b
sai ram kaushik <sairamkaushik1@gmail.com>
2021-07-05 16:09:17 +0530

    modified css in about.css, and added about.php

--- about.php
+++ about.php
@@ -8,6 +8,12 @@
     <title>About</title>
     <link rel="stylesheet" href="css/about.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Orbitron:wght@900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
@@ -16,16 +22,25 @@
             <div class="menu">
                 <ul>
 
-                    <li><a href="./index.html">Home</a></li>
-                    <li><a href="./active.html">Active Caese</a></li>
+                    <li><a href="./index.php">Home</a></li>
+                    <li><a href="./active.php">Active Cases</a></li>
                     <li><a href="./about.php">About</a></li>
                 </ul>
             </div>
         </nav>
-        <img src="./images/IMG_20190323_114833.jpg" width="45%" height="60%">
+        <img src="./images/IMG_20190323_114833.jpg" width="20%" height="30%">
+        <div class="developer">
+            <h2>SAI RAM KAUSHIK</h2>
+        </div>
         <svg width="100vw" height="100vh">
-            <text x="37%" y="50%">DEVELOPER</text>
+            <text x="37%" y="10%">DEVELOPER</text>
         </svg>
+        <a href="https://www.instagram.com/sairamkaushik/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"
+                id="insta"></i></a>
+        <a href="https://github.com/sai-ram-kaushik" target="_blank"><i class="fa fa-github" aria-hidden="true"
+                id="github"></i></a>
+        <a href="https://www.linkedin.com/in/sairam-kaushik-711a40210/" target="_blank"><i class="fa fa-linkedin"
+                aria-hidden="true" id="linkedin"></i></a>
     </header>
 </body>
 <script src="./js/index.js"></script>
