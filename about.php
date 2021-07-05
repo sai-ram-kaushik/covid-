a2171ae118ccf49cd7dfddbcc904208166016d99
sai ram kaushik <sairamkaushik1@gmail.com>
2021-07-05 13:35:49 +0530

    modified all html, css and php files

---
+++ about.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="css/about.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="menu">
+                <ul>
+
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="./active.html">Active Caese</a></li>
+                    <li><a href="./about.php">About</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- <img src="./images/IMG_20190323_114833.jpg" width="45%" height="60%"> -->
+        <svg width="100vw" height="100vh">
+            <text x="37%" y="50%">DEVELOPER</text>
+        </svg>
+    </header>
+</body>
+<script src="./js/index.js"></script>
+
+</html>
